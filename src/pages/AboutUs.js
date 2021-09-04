2dbd7f167fe1e6c799a2565367fae294e4e2b51a
nicochamut <nicochamut@gmail.com>
2021-09-04 19:34:17 -0300

    home2 section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,10 +1,14 @@
 import React from "react";
 import AboutSection from "../components/AboutSection";
+import ServicesSection from "../components/ServicesSection";
 
 
 const AboutUs = () => {
     return(
+        <div>
         <AboutSection />
+        <ServicesSection />
+        </div>
     )
 }
 
