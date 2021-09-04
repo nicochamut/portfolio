6d09a8b6576b935508cb39aff96da71aae94f692
nicochamut <nicochamut@gmail.com>
2021-09-04 19:18:29 -0300

    About us section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
+import AboutUs from "./pages/AboutUs";
+
 
 function App() {
   return (
     <div className="App">
       <h1>Film s</h1>
+      <AboutUs />
     </div>
   );
 }
