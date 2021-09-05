868008a7f4ed00142c204cddeda4363425ad334e
nicochamut <nicochamut@gmail.com>
2021-09-05 20:28:23 -0300

    Styled Components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import AboutUs from "./pages/AboutUs";
+import GlobalStyle from "./components/GlobalStyle"
 
 
 function App() {
   return (
     <div className="App">
-      <h1>Film s</h1>
+      <GlobalStyle />
       <AboutUs />
     </div>
   );
--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,9 +1,12 @@
 import React from "react";
 import home1 from "../img/home1.png"
+//styled
+import styled from "styled-components";
+
 const AboutSection = () => {
     return(
-         <div>
-             <div className="description">
+         <About>
+             <Description>
                  <div className="title">
                      <div className="hide">
                          <h2>We work to make</h2>
@@ -15,14 +18,39 @@ const AboutSection = () => {
                          <h2>come true.</h2>
                      </div>
                  </div>
-                 <p>Contact us for any photography ideas that you have.
+                 <p> 
+                     Contact us for any photography ideas that you have.
                      We have professionals with amazing skills
                  </p>
                  <button>Contact us</button>
-             </div>
-             <img src={home1} alt="guy with camera"/>
-         </div>
+             </Description>
+             <Image>
+                  <img src={home1} alt="guy with camera" />  
+             </Image> 
+         </About>
     )
 }
+
+//Styled Component
+const About = styled.div`
+  min-height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5rem 10rem;
+  color: white;
+`
+
+const Description = styled.div`
+flex: 1;
+padding-right: 5rem;
+h2{
+    font-weight: lighter;
+}
+`
+const Image = styled.div`
+flex: 1;
+`
+
 
 export default AboutSection 
