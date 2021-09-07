5636bada02eab3f9082b3005c1f4a889039a9283
nicochamut <nicochamut@gmail.com>
2021-09-07 04:11:43 -0300

    animation

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -3,24 +3,27 @@ import home1 from "../img/home1.png"
 //styled
 import styled from "styled-components";
 import { About, Description, Hide, Image } from "../style";
+// Framer motion
+import { motion } from "framer-motion";
+
 
 const AboutSection = () => {
   return (
     <About>
       <Description>
-        <div className="title">
+        <motion.div className="title">
           <Hide>
-            <h2>We work to make</h2>
+            <motion.h2>We work to make</motion.h2>
           </Hide>
           <Hide>
-            <h2>
+            <motion.h2>
               your <span>dreams</span>{" "}
-            </h2>
+            </motion.h2>
           </Hide>
           <Hide>
-            <h2>come true.</h2>
+            <motion.h2>come true.</motion.h2>
           </Hide>
-        </div>
+        </motion.div>
         <p>
           Contact us for any photography ideas that you have. We have
           professionals with amazing skills
